91a624fd51ff9b84c61e7f7510ebe140ea657b1d
Sumsan38 <ndanl4647@gmail.com>
2022-01-10 22:36:31 +0900

    인텔리제이 커밋 테스트

---
+++ src/main/java/com/tp/PlsFridge/PlsFridgeApplication.java
@@ -0,0 +1,13 @@
+package com.tp.PlsFridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlsFridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlsFridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/PlsFridge/PlsFridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.PlsFridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlsFridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
