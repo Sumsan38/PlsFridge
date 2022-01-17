a898447fb6f0b68e9cd084b6bacfd914e3da98c2
Sumsan38 <ndadnl4647@gmail.com>
2022-01-17 21:28:33 +0900

    기본 페이지 셋팅

--- plsfridge-react/src/AxiosTest.js
+++ plsfridge-react/src/AxiosTest.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import customAxios from './customAxios';
 
 // 기존의 App.js에 있던 파일
--- plsfridge-react/src/index.js
+++ plsfridge-react/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+// Test용 빈 페이지
+import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// 메인 페이지를 통해 Axios 연결
+/*
+import reportWebVitals from './reportWebVitals';
+import TestApp from './AxiosTest';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TestApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+*/
