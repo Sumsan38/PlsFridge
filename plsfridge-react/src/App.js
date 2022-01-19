c6dcd895aadfd64d1774564e196220e86039b79b
Sumsan38 <ndadnl4647@gmail.com>
2022-01-19 23:46:33 +0900

    커밋 테스트

--- plsfridge-react/src/App.js
+++ plsfridge-react/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     };
     countUp = () => {
         this.setState({
-            count: this.state.count + 1
+            count: this.state.count + 2
         });
     };
 
