7f175bcbb34254b047a2a61913911ae77bbfbed7
HY-LEE1005 <hyeyeon.lee1005@gmail.com>
2022-01-25 16:59:21 +0900

    마우스 오버시 하위메뉴 표시하는 메뉴바 (왼쪽) 테스트

--- plsfridge-react/src/index.js
+++ plsfridge-react/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import "./index.css";
 import App from './App';
 import BottomNav from './BottomNav';
+import Menu from './Menu';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <BottomNav /> {/* nav 가 항상 떠있도록 index.js에서 호출 */}
+      <Menu />
+      {/* <BottomNav /> */} {/* nav 가 항상 떠있도록 index.js에서 호출 */}
       <App /> {/* bottom 에 따라 바뀌는 본문 */}
     </BrowserRouter>
   </React.StrictMode>,
