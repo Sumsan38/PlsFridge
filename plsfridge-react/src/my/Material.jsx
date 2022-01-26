c6819add8ba00bb6e982762f292e6b443fe85bbf
Sumsan38 <ndanl4647@gmail.com>
2022-01-26 17:03:37 +0900

    메뉴바 테스트

--- plsfridge-react/src/my/Material.jsx
+++ plsfridge-react/src/my/Material.jsx
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 class Material extends Component {
     render (){
+        let value = 1;
         return (
             <div>
                 내 재료 리스트 입니다.
+                {
+                    1 + 1 === 2 ? <div>true</div> : <div>false</div>
+                }
+                    <div>
+                    {
+                        (function () {
+                            if(value === 1) return (<div>value 값은 {value}입니다</div>);
+                            if(value === 2) return (<div>value 값은 2입니다</div>);
+                            }
+                        )()
+                    }
+                    </div>
             </div>
         );
     }
